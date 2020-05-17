8b626249cd61fcf7e6ba65252a14410796904ab3
MintStudios <mintstudios.dev@gmail.com>
2020-05-17 14:57:11 -0400

    Cleans code for better understanding

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -11,6 +11,7 @@ func _ready():
 	title_update()
 
 func title_update():
+	# This sets the title for the current title
 	OS.set_window_title('Signum - ' + current_file)
 
 # File Menu IDs:
@@ -27,53 +28,74 @@ func title_update():
 func file_item_pressed(id):
 	match id:
 		0:
+			# Opens file select dialog
 			open_file_dialog.popup()
 		1:
+			# Opens save file dialog
 			save_as_file_dialog.popup()
 		2:
+			# Closes the program
 			get_tree().quit()
 		3:
+			# Creates file
 			new_file()
 		4:
+			# Saves file without changing the name
 			save_file()
 
 func help_item_pressed(id):
 	match id:
 		0:
+			# Opens about popup
 			about_popup.popup()
 		1:
+			# Opens the github page in the browser
 			OS.shell_open('https://github.com/MintStudios/Signum')
 
-
-
+# Creates a new file
 func new_file():
 	current_file = UNTITLED
 	title_update()
+	# Resets the text back to nothing
 	$TextEdit.text = ''
 
+# Opens an existing file
 func open_file_selected(path):
+	# Creates and reads the file
 	var file = File.new()
 	file.open(path, 1)
+	# Makes the TextEdit text the same as the file's
 	$TextEdit.text = file.get_as_text()
+	# Closes to prevent memory leaks
 	file.close()
+	# Changes the title to the file path
 	current_file = path
 	title_update()
 
+# Saves the file as a file type
 func save_as_file_selected(path):
+	# Creates and writes the file
 	var file = File.new()
 	file.open(path, 2)
 	file.store_string($TextEdit.text)
+	# Closes to prevent memory leaks
 	file.close()
+	# Changes the title to the file path
 	current_file = path
 	title_update()
 
 func save_file():
+	# Changes the title to the file path
 	var path = current_file
+	# Checks if the file hasn't been created. If it has, then it triggers the 'Save As` Dialog
 	if path == UNTITLED:
 		save_as_file_dialog.popup()
+	# If the file exists, writes the file.
 	else:
 		var file = File.new()
 		file.open(path, 2)
 		file.store_string($TextEdit.text)
+		# Closes to prevent memory leaks
 		file.close()
+		# Changes the title to the file path
 		current_file = path
--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -14,20 +14,26 @@ onready var help_menu = get_node('HelpMenu')
 # About = 0
 # Github Page = 1
 
-func _ready():	
+func _ready():
+	# Sets all the shortcuts
 	file_menu.get_popup().set_item_shortcut(0, set_shortcut(KEY_O), true)
 	file_menu.get_popup().set_item_shortcut(1, set_shortcut(KEY_E), true)
 	file_menu.get_popup().set_item_shortcut(4, set_shortcut(KEY_Q), true)
 	file_menu.get_popup().set_item_shortcut(2, set_shortcut(KEY_N), true)
 	file_menu.get_popup().set_item_shortcut(3, set_shortcut(KEY_S), true)
 	
+	# Connects the signals of the items to the main script
 	file_menu.get_popup().connect('id_pressed', get_parent(), 'file_item_pressed')
 	help_menu.get_popup().connect('id_pressed', get_parent(), 'help_item_pressed')
 
+# Function that makes a shortcut
 func set_shortcut(key):
+	# Creates ShortCut and InputKeyEvent
 	var shortcut = ShortCut.new()
 	var inputeventkey = InputEventKey.new()
+	# Sets the scanned key and uses control as the preceding command
 	inputeventkey.set_scancode(key)
 	inputeventkey.control = true
+	# Makes the final shortcut and returns it
 	shortcut.set_shortcut(inputeventkey)
 	return shortcut
